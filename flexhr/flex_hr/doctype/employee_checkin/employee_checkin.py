8fef7497a4c5e37fe5384043b7137b5601b1bf1a
ashish-greycube <mr.ashish.shah@gmail.com>
2018-11-16 10:32:21 +0530

    attendance data exp

--- flexhr/flex_hr/doctype/employee_checkin/employee_checkin.py
+++ flexhr/flex_hr/doctype/employee_checkin/employee_checkin.py
@@ -10,122 +10,107 @@ from werkzeug.wrappers import Response
 
 class EmployeeCheckin(Document):
 	def validate(self):
-		if self.employee:
-			attendance = frappe.db.get_list("Attendance",
-				{"employee": self.employee,
-				"attendance_date": datetime.datetime.now()},
-				["name"]
-			)
-			if not attendance:
-				employee_doc = frappe.get_doc("Employee", self.employee)
-				## Create Employee Attendance
-				employee_attendance = frappe.new_doc("Attendance")
-				employee_attendance.company = employee_doc.company
-				employee_attendance.employee = employee_doc.name
-				employee_attendance.employee_name = employee_doc.employee_name
-				employee_attendance.status = "Present"
-				employee_attendance.attendance_date =  datetime.datetime.now()
-				employee_attendance.department = employee_doc.department
-				employee_attendance.save()
-				employee_attendance.submit()
+		pass
+		# if self.employee:
+		# 	attendance = frappe.db.get_list("Attendance",
+		# 		{"employee": self.employee,
+		# 		"attendance_date": datetime.datetime.now()},
+		# 		["name"]
+		# 	)
+		# 	if not attendance:
+		# 		employee_doc = frappe.get_doc("Employee", self.employee)
+		# 		## Create Employee Attendance
+		# 		employee_attendance = frappe.new_doc("Attendance")
+		# 		employee_attendance.company = employee_doc.company
+		# 		employee_attendance.employee = employee_doc.name
+		# 		employee_attendance.employee_name = employee_doc.employee_name
+		# 		employee_attendance.status = "Present"
+		# 		employee_attendance.attendance_date =  datetime.datetime.now()
+		# 		employee_attendance.department = employee_doc.department
+		# 		employee_attendance.save()
+		# 		employee_attendance.submit()
 
-		if self.out_time:
-			in_time = datetime.datetime(*time.strptime(str(self.in_time), "%Y-%m-%d %H:%M:%S")[:6])
-			out_time = datetime.datetime(*time.strptime(str(self.out_time), "%Y-%m-%d %H:%M:%S")[:6])
-			if in_time > out_time:
-				frappe.throw("Out-time must be later than In-time.")
-			else:
-				duration = out_time - in_time
-				self.duration = duration
+		# if self.out_time:
+		# 	in_time = datetime.datetime(*time.strptime(str(self.in_time), "%Y-%m-%d %H:%M:%S")[:6])
+		# 	out_time = datetime.datetime(*time.strptime(str(self.out_time), "%Y-%m-%d %H:%M:%S")[:6])
+		# 	if in_time > out_time:
+		# 		frappe.throw("Out-time must be later than In-time.")
+		# 	else:
+		# 		duration = out_time - in_time
+		# 		self.duration = duration
+
 
 @frappe.whitelist(allow_guest=True)
-def punch_in(userid):
-		response = Response()
-		response.mimetype = 'text/plain'
-		response.charset = 'utf-8'
-		response.data = 'OK'
-		return response
-	# if "Auto Attendance" in frappe.get_roles(frappe.session.user):
-		employee = frappe.get_value('Employee', {'attendance_user_id': userid}, "name")
-		if employee:
-			employee_doc = frappe.get_doc("Employee", employee)
-			if employee_doc.status == 'Left':
-				return employee_not_active(userid)
-			else:
-				if emp_checkin_exist(employee):
-					return [{
-						"status":"error",
-						"error_message":"Already punched in."
-					}]
-				else:
-					check_in = frappe.new_doc("Employee Checkin")
-					# check_in.company = employee_doc.company
-					check_in.employee = employee
-					check_in.employee_name = employee_doc.employee_name
-					check_in.in_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-					# check_in.save()
-					response = Response()
-					response.mimetype = 'text/plain'
-					response.charset = 'utf-8'
-					response.data = 'OK'
-					return response
-		else:
-			return rfid_unknown(userid)
-	# else:
-		# return user_not_authorized()
+def punch_in(att_type,stgid,att_time,userid,auth_token=None):
+	create_checkin_record(att_type,stgid,att_time,userid,auth_token)
+	return standard_response()
 
-@frappe.whitelist(allow_guest=False)
-def punch_out(rfid_tag):
-	if "Auto Attendance" in frappe.get_roles(frappe.session.user):
-		employee = frappe.get_value('Employee', {'rfid_tag': rfid_tag}, "name")
-		if employee:
-			employee_doc = frappe.get_doc("Employee", employee)
-			if employee_doc.status == 'Left':
-				return employee_not_active(rfid_tag)
-			else:
-
-				if emp_checkin_exist(employee):
-					checkout = frappe.db.sql("""Select name from `tabEmployee Checkin` where employee = %s and out_time is null""",(employee))
-					checkout_rec = checkout[0][0]
-					emp_check_doc = frappe.get_doc("Employee Checkin", (checkout_rec))
-					emp_check_doc.out_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-					emp_check_doc.save()
-					emp_check_doc.submit()
-					return [{
-						"status":"success",
-						"employee_name":employee_doc.employee_name,
-						"rfid_tag":employee_doc.rfid_tag,
-						"in_time":emp_check_doc.in_time,
-						"out_time":emp_check_doc.out_time,
-						"duration":emp_check_doc.duration
-					}]
-				else:
-					return [{
-						"status":"error",
-						"error_message":"Not punched in."
-					}]
+@frappe.whitelist(allow_guest=True)
+def punch_in1(att_type,stgid,att_time,userid,auth_token=None):
+	employee = frappe.get_value('Employee', {'attendance_user_id': userid}, "name")
+	if auth_token:
+		if auth_token != frappe.db.get_single_value("Attendance Device Settings", "auth_token"):
+			remark = "Authorization Token Doesn't Match"
+			status = 'Fail'
+			create_checkin_record(att_type,stgid,att_time,userid,remark,status,auth_token,employee)
+			return standard_response()
 		else:
-			return rfid_unknown(rfid_tag)
+			if employee:
+				employee_doc = frappe.get_doc("Employee", employee)
+				if employee_doc.status == 'Left':
+					remark = 'Employee Not Active. Left'
+					status = 'Fail'
+					create_checkin_record(att_type,stgid,att_time,userid,remark,status,auth_token,employee)
+					return standard_response()
+				else:
+					remark = ''
+					status = 'Pass'
+					create_checkin_record(att_type,stgid,att_time,userid,remark,status,auth_token,employee)
+					return standard_response()
+			else:
+				remark = "Attendance Userid Doesn't Match With Any Employee"
+				status = 'Fail'
+				create_checkin_record(att_type,stgid,att_time,userid,remark,status,auth_token,employee=None)
+				return standard_response()
 	else:
-		return user_not_authorized()
+		remark = 'Authorization Token Not Found'
+		status = 'Fail'
+		create_checkin_record(att_type,stgid,att_time,userid,remark,status,auth_token,employee)
+		return standard_response()
 
-def rfid_unknown(rfid_tag):
-	return [{
-		"status":"error",
-		"error_message":"RFID Tag unknown."
-	}]
+def standard_response():
+	response = Response()
+	response.mimetype = 'text/plain'
+	response.charset = 'utf-8'
+	response.data = 'OK'
+	return response
 
-def employee_not_active(rfid_tag):
-	return [{
-		"status":"error",
-		"error_message":"Employee not active."
-	}]
 
-def emp_checkin_exist(employee):
-	return frappe.db.sql("""Select name from `tabEmployee Checkin` where employee = %s and out_time is null""",(employee))
+@frappe.whitelist(allow_guest=True)
+def create_checkin_record(att_type,stgid,att_time,userid,auth_token=None,employee=None,remark=None,status=None):
+	check_in = frappe.new_doc("Employee Checkin")
+	check_in.employee = employee
+	check_in.attendance_user_id = userid
+	check_in.device_id=stgid
+	check_in.auth_token=auth_token
+	check_in.att_type = att_type
+	check_in.att_time = datetime.datetime.fromtimestamp(int(att_time)).strftime("%Y-%m-%d %H:%M:%S")
+	check_in.status=status
+	check_in.remark=remark
+	check_in.insert(ignore_permissions=True)
+	check_in.submit()
+	print check_in.name
+	print '-------------------------------------'
 
-def user_not_authorized():
-	return [{
-		"status":"error",
-		"error_message":"User not authorized."
-	}]
+@frappe.whitelist(allow_guest=True)
+def punch_out(att_type,stgid,att_time,userid,auth_token=None):
+	check_in = frappe.new_doc("Attendance Device Log")
+	check_in.attendance_user_id = userid
+	check_in.device_id=stgid
+	check_in.auth_token=auth_token
+	check_in.att_type = att_type
+	check_in.insert(ignore_permissions=True)
+	print check_in.name
+	print '-----------------------------------'
+	return standard_response()
+
