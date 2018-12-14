2031409b91d3e4be4804c8e70c68f7aa319cc752
ashish-greycube <mr.ashish.shah@gmail.com>
2018-12-14 11:02:00 +0530

    rename issue

--- flexhr/flex_hr/doctype/employee_checkin_uploader/employee_checkin_uploader.js
+++ flexhr/flex_hr/doctype/employee_checkin_uploader/employee_checkin_uploader.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2018, GreyCube Technologies and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Employee Checkin Upload', {
+frappe.ui.form.on('Employee Checkin Uploader', {
 	refresh: function(frm) {
 
 	}
